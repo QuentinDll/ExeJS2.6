42246f3c19a71da12d9f92eaea983d7b91604d8e
QuentinDll <quentin.delplace13@gmail.com>
2018-06-25 09:50:42 +0200

    creation function switch img on mouse hover and reset img when out

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+//fonction souris au dessus
+function on(){
+  document.getElementById('image1').src = 'assets/images/image1_2.jpg';
+  document.getElementById('image2').src = 'assets/images/image2_2.jpg';
+  document.getElementById('image3').src = 'assets/images/image3_2.jpg';
+  document.getElementById('image4').src = 'assets/images/image4_2.jpg';
+  document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+}
+//fonction souris en dehort
+function off(){
+  document.getElementById('image1').src = 'assets/images/image1.jpg';
+  document.getElementById('image2').src = 'assets/images/image2.jpg';
+  document.getElementById('image3').src = 'assets/images/image3.jpg';
+  document.getElementById('image4').src = 'assets/images/image4.jpg';
+  document.getElementById('image5').src = 'assets/images/image5.jpg';
+}
