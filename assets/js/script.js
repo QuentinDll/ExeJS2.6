b7b68e499967c92c41e4d91fecbf4c1cd36bb4fa
QuentinDll <quentin.delplace13@gmail.com>
2018-06-25 14:21:51 +0200

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,8 @@
-//fonction souris au dessus
-function on(){
-  document.getElementById('image1').src = 'assets/images/image1_2.jpg';
-  document.getElementById('image2').src = 'assets/images/image2_2.jpg';
-  document.getElementById('image3').src = 'assets/images/image3_2.jpg';
-  document.getElementById('image4').src = 'assets/images/image4_2.jpg';
-  document.getElementById('image5').src = 'assets/images/image5_2.jpg';
+//fonction souris dessus
+function on(id){
+  document.getElementById(id).src = 'assets/images/' + id + '_2.jpg';
 }
-//fonction souris en dehort
-function off(){
-  document.getElementById('image1').src = 'assets/images/image1.jpg';
-  document.getElementById('image2').src = 'assets/images/image2.jpg';
-  document.getElementById('image3').src = 'assets/images/image3.jpg';
-  document.getElementById('image4').src = 'assets/images/image4.jpg';
-  document.getElementById('image5').src = 'assets/images/image5.jpg';
+//fonction souris en dehors
+function out(id){
+  document.getElementById(id).src = 'assets/images/' + id + '.jpg';
 }
